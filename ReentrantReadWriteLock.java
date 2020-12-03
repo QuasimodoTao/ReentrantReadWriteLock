099db2c7295603f4b38785b47f1ff4749919089a
Quasimodo <44406081+QuasimodoTao@users.noreply.github.com>
2020-12-03 18:20:26 +0800

    Update ReentrantReadWriteLock.java

--- ReentrantReadWriteLock.java
+++ ReentrantReadWriteLock.java
@@ -1,5 +1,21 @@
-package learn0;
+/*
+	ReentrantReadWriteLock.java
+	Copyright (C) 2020  Quasimodo 
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
 
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -197,6 +213,13 @@ public class ReentrantReadWriteLock {
 			}
 			else {//Upgrade.
         //ID_XOR and ID_ADD to ensure only current thread holding reader-lock(s).
+				/*
+				if((ID_XOR == 0 || ID_XOR == CurThreadId) &&
+					curThreadId * readers == ID_ADD){
+						Permit thread get a writer-lock;
+					}
+				*/
+				
 				if(ID_XOR == 0 || ID_XOR == curThreadId) {
 					UINT124 mul = new UINT124();
 					mul.mul(curThreadId, readers);
